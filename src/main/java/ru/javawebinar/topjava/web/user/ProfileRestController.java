7fc2e0f6e2f0ce57249c37b28dc7583ad8a06d18
Vladimir <lakomy.vova@gmail.com>
2016-07-21 01:33:20 +0300

    7.15

--- src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileRestController.java
@@ -31,4 +31,9 @@ public class ProfileRestController extends AbstractUserController {
     public void update(@RequestBody User user) {
         super.update(user, AuthorizedUser.id());
     }
+
+    @RequestMapping(value = "/text", method = RequestMethod.GET)
+    public String testUTF() {
+        return "Русский текст";
+    }
 }
