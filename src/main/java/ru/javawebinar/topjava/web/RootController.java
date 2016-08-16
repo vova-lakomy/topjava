9db9a1f51cb3e888209fa98a6341664d5e07b2e0
Vladimir <lakomy.vova@gmail.com>
2016-08-16 13:00:40 +0300

    10.06

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -1,23 +1,33 @@
 package ru.javawebinar.topjava.web;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.to.UserTo;
+import ru.javawebinar.topjava.web.user.AbstractUserController;
+
+import javax.validation.Valid;
 
 /**
  * User: gkislin
  * Date: 22.08.2014
  */
 @Controller
-public class RootController {
+public class RootController extends AbstractUserController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String root() {
         return "redirect:meals";
     }
 
+//    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     public String userList() {
         return "userList";
@@ -36,5 +46,23 @@ public class RootController {
         model.put("error", error);
         model.put("message", message);
         return "login";
+    }
+
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public String profile() {
+        return "profile";
+    }
+
+    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    public String updateProfile(@Valid UserTo userTo, BindingResult result, SessionStatus status) {
+        if (result.hasErrors()) {
+            return "profile";
+        } else {
+            userTo.setId(AuthorizedUser.id());
+            super.update(userTo);
+            AuthorizedUser.get().update(userTo);
+            status.setComplete();
+            return "redirect:meals";
+        }
     }
 }
--- src/main/webapp/WEB-INF/jsp/fragments/bodyHeader.jsp
+++ src/main/webapp/WEB-INF/jsp/fragments/bodyHeader.jsp
@@ -1,5 +1,7 @@
 <%@page contentType="text/html" pageEncoding="UTF-8" %>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 
 <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -8,8 +10,13 @@
 
         <div class="collapse navbar-collapse">
             <form class="navbar-form navbar-right">
-                <a class="btn btn-info" role="button" href="users"><fmt:message key="users.title"/></a>
-                <a class="btn btn-primary" role="button" href="logout"><fmt:message key="app.logout"/></a>
+                <sec:authorize access="isAuthenticated()">
+                    <sec:authorize access="hasRole('ROLE_ADMIN')">
+                        <a class="btn btn-info" role="button" href="users"><fmt:message key="users.title"/></a>
+                    </sec:authorize>
+                    <a class="btn btn-info" role="button" href="profile">${userTo.name} profile</a>
+                    <a class="btn btn-primary" role="button" href="logout"><fmt:message key="app.logout"/></a>
+                </sec:authorize>
             </form>
         </div>
     </div>
